e1b12c5a8a07e53c2c9ee4aed2349b16268e3b69
Jonas Kraft <32849404+JohnnyVim@users.noreply.github.com>
2023-05-11 16:50:26 +0200

    Add second dependency

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/JohnnyVim/renovate-yaml-repro
 go 1.18
 
 require gopkg.in/yaml.v2 v2.4.0
+
+require golang.org/x/text v0.9.0
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	yaml.Marshal(1)
+	msg := cases.Title(language.English).String("Renovate is cool!")
+	yaml.Marshal(msg)
 }
